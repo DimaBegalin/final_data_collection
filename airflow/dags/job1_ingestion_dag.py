7fb14b5db67a3c9751008d82061399be7313c214
dimka077 <60312284+DimaBegalin@users.noreply.github.com>
2025-12-19 22:03:36 +0500

    Add default_args to job1_ingestion_dag
    Added default arguments for the DAG, including retry settings and owner.

--- airflow/dags/job1_ingestion_dag.py
+++ airflow/dags/job1_ingestion_dag.py
@@ -1,11 +1,21 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from datetime import datetime
+from datetime import datetime, timedelta
 import sys, os, time
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 from src.job1_producer import run_producer
 
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2025, 12, 20),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 3,                            
+    'retry_delay': timedelta(seconds=15),     
+}
 
 def continuous_streaming():
     while True:
@@ -14,6 +24,7 @@ def continuous_streaming():
 
 with DAG(
     'dag1_continuous_ingestion',
+    default_args=default_args,
     start_date=datetime(2025, 12, 19),
     schedule_interval=None,
     catchup=False
